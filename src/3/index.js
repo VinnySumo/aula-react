d403353fa032482cacd374a786e46afeffbae161
VinnySumo <vinnyrafael015@hotmail.com>
2024-03-20 21:22:27 -0300

    aula 3

---
+++ src/3/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'; 
+
+import styles from './styles';
+
+export default function Mensagem () { 
+
+    return(
+        <View style={styles.container}>
+            <Text>
+                Aula 3
+            </Text>
+        </View>
+    );
+}
